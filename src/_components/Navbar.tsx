8f4849407ed8e9207c2ecfa1d2270578ed0f4dad
kaizene <mikkelnothlev@outlook.com>
2024-10-01 12:12:56 +0200

    navbar done for now

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -47,60 +47,97 @@ const Navbar: React.FC<NavbarProps> = ({ setFooterHidden }) => {
     }, []);
 
     return (
-        <nav className="md:hidden top-0 left-0 z-10 p-4 m-2 relative">
+        <>
             {/* Mobile Navbar */}
-            <div className="flex items-center">
-                <HiMiniBars3BottomRight className="text-black dark:text-white text-xl" onClick={toggleMenu} />
+            <nav className="md:hidden top-0 left-0 z-10 p-4 m-2 relative">
+                <div className="flex items-center">
+                    <HiMiniBars3BottomRight className="text-black dark:text-white text-xl" onClick={toggleMenu} />
+                    <DarkMode className="ml-4"/>
+                </div>
+                <div className="fixed flex flex-col text-center z-10 right-0 top-0 h-screen w-0 overflow-x-hidden transition-all duration-300 ease-in bg-white dark:bg-black" ref={menuBlock}>
+                    <img src={isDarkMode ? "/dark-logo.png" : "/logo.png"} alt="Ace Studios Logo" className="w-1/2 mx-auto mt-2" />
+                    <div className="flex flex-col text-center mb-4 md:mb-0">
+                        <a href="" className="block mb-4 dark:text-white font-semibold">About Us</a>
+                        <a href="" className="block mb-4 dark:text-white font-semibold">Our Team</a>
+                        <a href="" className="block mb-4 dark:text-white font-semibold">Store</a>
+                        <a href="" className="block mb-4 dark:text-white font-semibold">Contact</a>
+                        <a href="" className="block mb-4 dark:text-white">Careers</a>
+                    </div>
+                    <div className="flex flex-col text-center mb-4 md:mb-0">
+                        <a href="" className="block mb-4 dark:text-white">Projects</a>
+                        <a href="" className="block mb-4 dark:text-white">Upcoming Releases</a>
+                        <a href="" className="block mb-4 dark:text-white">Awards</a>
+                        <a href="" className="block mb-4 dark:text-white">News</a>
+                        <a href="" className="block mb-4 dark:text-white">Events</a>
+                    </div>
+                    <div className="flex flex-col text-center mmb-4 md:mb-0">
+                        <a href="" className="block mb-4 dark:text-white">Privacy Policy</a>
+                        <a href="" className="block mb-4 dark:text-white">Terms of Service</a>
+                        <a href="" className="block mb-4 dark:text-white">Cookie Policy</a>
+                        <a href="" className="block mb-4 dark:text-white">Accessibility</a>
+                    </div>
+                    <div className="mt-auto mb-10">
+                        <div className="flex justify-center space-x-4 mb-6">
+                            <a href="" className="dark:text-white">
+                                <FaFacebook className="text-black dark:text-white text-xl" />
+                            </a>
+                            <a href="" className="dark:text-white">
+                                <FaInstagram className="text-black dark:text-white text-xl" />
+                            </a>
+                            <a href="" className="dark:text-white">
+                                <FaLinkedinIn className="text-black dark:text-white text-xl" />
+                            </a>
+                        </div>
+                        <div className="flex justify-center space-x-4">
+                            <a href="" className="dark:text-white">
+                                <FaTiktok className="text-black dark:text-white text-xl" />
+                            </a>
+                            <a href="" className="dark:text-white">
+                                <FaXTwitter className="text-black dark:text-white text-xl" />
+                            </a>
+                            <a href="" className="dark:text-white">
+                                <SiGmail className="text-black dark:text-white text-xl" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            {/* Desktop Navbar */}
+            <nav className="hidden md:flex justify-between items-center p-4 bg-white dark:bg-black relative">
+                <div className="flex space-x-8 ml-8">
+                    <a href="" className="hover:text-gray-500 dark:text-white dark:hover:text-slate-200">About Us</a>
+                    <a href="" className="hover:text-gray-500 dark:text-white dark:hover:text-slate-200">Our Team</a>
+                    <a href="" className="hover:text-gray-500 dark:text-white dark:hover:text-slate-200">Store</a>
+                    <a href="" className="hover:text-gray-500 dark:text-white dark:hover:text-slate-200">Contact</a>
+                    <a href="" className="hover:text-gray-500 dark:text-white dark:hover:text-slate-200">Careers</a>
+                </div>
+
                 <DarkMode className="ml-4"/>
-            </div>
-            <div className="fixed flex flex-col text-center z-10 right-0 top-0 h-screen w-0 overflow-x-hidden transition-all duration-300 ease-in bg-white dark:bg-black" ref={menuBlock}>
-                <img src={isDarkMode ? "/dark-logo.png" : "/logo.png"} alt="Ace Studios Logo" className="w-1/2 mx-auto mt-2" />
-                <div className="flex flex-col text-center mb-4 md:mb-0">
-                    <a href="" className="block mb-4 dark:text-white font-semibold">About Us</a>
-                    <a href="" className="block mb-4 dark:text-white font-semibold">Our Team</a>
-                    <a href="" className="block mb-4 dark:text-white font-semibold">Store</a>
-                    <a href="" className="block mb-4 dark:text-white font-semibold">Contact</a>
-                    <a href="" className="block mb-4 dark:text-white">Careers</a>
+
+                <div className="flex flex-col items-center space-y-4 fixed right-4 top-1/2 transform -translate-y-1/2">
+                    {/* Social Media Icons */}
+                    <a href="" className="dark:text-white">
+                        <FaFacebook className="text-black dark:text-white text-xl" />
+                    </a>
+                    <a href="" className="dark:text-white">
+                        <FaInstagram className="text-black dark:text-white text-xl" />
+                    </a>
+                    <a href="" className="dark:text-white">
+                        <FaLinkedinIn className="text-black dark:text-white text-xl" />
+                    </a>
+                    <a href="" className="dark:text-white">
+                        <FaTiktok className="text-black dark:text-white text-xl" />
+                    </a>
+                    <a href="" className="dark:text-white">
+                        <FaXTwitter className="text-black dark:text-white text-xl" />
+                    </a>
+                    <a href="" className="dark:text-white">
+                        <SiGmail className="text-black dark:text-white text-xl" />
+                    </a>
                 </div>
-                <div className="flex flex-col text-center mb-4 md:mb-0">
-                    <a href="" className="block mb-4 dark:text-white">Projects</a>
-                    <a href="" className="block mb-4 dark:text-white">Upcoming Releases</a>
-                    <a href="" className="block mb-4 dark:text-white">Awards</a>
-                    <a href="" className="block mb-4 dark:text-white">News</a>
-                    <a href="" className="block mb-4 dark:text-white">Events</a>
-                </div>
-                <div className="flex flex-col text-center mmb-4 md:mb-0">
-                    <a href="" className="block mb-4 dark:text-white">Privacy Policy</a>
-                    <a href="" className="block mb-4 dark:text-white">Terms of Service</a>
-                    <a href="" className="block mb-4 dark:text-white">Cookie Policy</a>
-                    <a href="" className="block mb-4 dark:text-white">Accessibility</a>
-                </div>
-                <div className="mt-auto mb-10">
-                    <div className="flex justify-center space-x-4 mb-6">
-                        <a href="" className="dark:text-white">
-                            <FaFacebook className="text-black dark:text-white text-xl" />
-                        </a>
-                        <a href="" className="dark:text-white">
-                            <FaInstagram className="text-black dark:text-white text-xl" />
-                        </a>
-                        <a href="" className="dark:text-white">
-                            <FaLinkedinIn className="text-black dark:text-white text-xl" />
-                        </a>
-                    </div>
-                    <div className="flex justify-center space-x-4">
-                        <a href="" className="dark:text-white">
-                            <FaTiktok className="text-black dark:text-white text-xl" />
-                        </a>
-                        <a href="" className="dark:text-white">
-                            <FaXTwitter className="text-black dark:text-white text-xl" />
-                        </a>
-                        <a href="" className="dark:text-white">
-                            <SiGmail className="text-black dark:text-white text-xl" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     )
 }
 
